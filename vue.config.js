a9d190d96a996e6dea27902fe9684a925bb41ee7
tuchongyang <779311998@qq.com>
2019-10-23 08:52:32 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const debug = process.env.NODE_ENV !== 'production'
-
+const serverUrl = process.env.VUE_APP_SERVER_URL || ''
 
 module.exports = {
     // // 基本路径
-    // publicPath: '/'+version,
+    publicPath: serverUrl || '/',
     // // 输出文件目录
-    // outputDir: version||'dist',
-    // assetsDir: 'assets', // 静态资源目录 (js, css, img, fonts)
+    outputDir: 'dist',
+    assetsDir: 'assets', // 静态资源目录 (js, css, img, fonts)
     // // eslint-loader 是否在保存的时候检查
     lintOnSave: false,
     // use the full build with in-browser compiler?
